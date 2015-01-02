428daff936bcc8804631c4588a3acb4d2dfe9c62
littleNghe <nghia89hoang@gmail.com>
2015-01-02 22:47:45 +0700

    using endpoint proto datastore

--- Endpoints/user_api.py
+++ Endpoints/user_api.py
@@ -3,11 +3,21 @@ from protorpc import messages
 from protorpc import message_type
 from protorpc import remote
 
-import Model.ndbUser
+from endpoints_proto_datastore.ndb import EndpointsModel
+from Model.ndbUser import TapUser
 
-
+@endpoints.api(name='userApi', version='v1', description='Tap user API')
 class UserApi(remote.Service):
-  pass
+  
+  @TapUser.query_method(path='tapusers', name='tapuser.list')
+  def TapUserList(self, query):
+    return query
+  
+  @TapUser.method(path='tapuser', http_method='POST', name='tapuser.insert')
+  def TapUserInsert(self, my_model):
+    my_model.put
+    return my_model
+    
     
 app = endpoints.api_server([
     UserApi
--- Model/ndbUser.py
+++ Model/ndbUser.py
@@ -1,10 +1,11 @@
 import webapp2
 import urllib2
-from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
+from endpoints_proto_datastore.ndb import EndpointsModel
 
-class TapUser(ndb.Model):
+# class TapUser(ndb.Model):
+class TapUser(EndpointsModel):
   userID = ndb.StringProperty(indexed = True)
-  email = ndb.StringProperty(indexed = True)
+  email = ndb.StringProperty(indexed = False)
   signupDate = ndb.DateTimeProperty(auto_now_add = True)
 
