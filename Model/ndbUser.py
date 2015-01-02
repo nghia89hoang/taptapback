379e36b288c984b5cdbe252b29178582a90ed51a
littleNghe <nghia89hoang@gmail.com>
2015-01-02 15:56:40 +0700

    fix error

--- Model/ndbUser.py
+++ Model/ndbUser.py
@@ -3,7 +3,7 @@ import urllib2
 from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
 
-class User(ndb.Model):
+class gUser(ndb.Model):
   userID = ndb.StringProperty(indexed = True)
   email = ndb.StringProperty(indexed = True)
   signupDate = ndb.DateTimeProperty(auto_now_add = True)
