0c6f842c0571e10d374f6608aa30cf6c986c1cd8
nghia89hoang <nghia89hoang@gmail.com>
2015-01-11 17:21:26 +0700

    - order game info list

--- Endpoints/tapgameinfo_api.py
+++ Endpoints/tapgameinfo_api.py
@@ -10,16 +10,21 @@ import CustomType
 
 @endpoints.api(name='tapGameInfoApi', version='v1', description='Tap Game API')
 class TapGameInfoApi(remote.Service):
-  
-  @TapGameInfo.query_method(path='tapgameinfoLst', name='tapgameinfo.list')
+
+  @TapGameInfo.query_method(query_fields=('order',), path='gameinfoLst', name='gameinfo.list')
   def TapGameInfoList(self, query):
     return query
-  
+
+
+#   @TapGameInfo.query_method(query_fields=('order',),path='gameinfoLstByLevel', name='gameinfo.listbylevel')
+#   def TapGameInfoListByLevel(self, query):
+#     return query
+
   # @TapUser.method(path='tapgameinfo', http_method='POST', name='tapgameinfo.insert', response_message = CustomType.StringMessage)
   # def TapUserInsert(self, new_user):
     # if TapUser.insert(new_user) == True:
       # new_tapgameinfo = TapGameInfo(parent=new_user.key)
-      # if TapGameInfo.insert(new_tapgameinfo) == True:      
+      # if TapGameInfo.insert(new_tapgameinfo) == True:
         # return CustomType.StringMessage(value = 'Insertion successed')
       # else:
         # return CustomType.StringMessage(value = 'Insertion Failed, can not insert TapGameInfor for user')
@@ -28,7 +33,7 @@ class TapGameInfoApi(remote.Service):
   # @TapUser.method(path='tapuser/del', http_method='POST', name='tapuser.del', response_message = CustomType.StringMessage)
   # def TapUserDelete(self, user_2del):
     # pass
-    
+
 # app = endpoints.api_server([
     # TapGameInfoApi
 # ])
--- Model/ndbTapGameInfo.py
+++ Model/ndbTapGameInfo.py
@@ -2,7 +2,22 @@ import webapp2
 import urllib2
 from google.appengine.ext import ndb
 from protorpc import message_types
+from protorpc import messages
 from endpoints_proto_datastore.ndb import EndpointsModel
+from endpoints_proto_datastore.ndb import EndpointsAliasProperty
+
+from Model.ndbUser import TapUser
+
+
+class Order(messages.Enum):
+  HI_LEVEL_ASC    =1
+  HI_LEVEL_DESC   =2
+  TOTAL_PRES_ASC  =3
+  TOTAL_PRES_DESC =4
+  TOTAL_REL_ASC   =5
+  TOTAL_REL_DESC  =6
+
+DEFAULT_ORDER = Order.HI_LEVEL_DESC
 
 class TapGameInfo(EndpointsModel):
   currLevel = ndb.IntegerProperty(indexed = True, default = 1)
@@ -10,6 +25,28 @@ class TapGameInfo(EndpointsModel):
   # totalGold = ndb.StringProperty(indexed = False, default = 0)
   totalRelics = ndb.IntegerProperty(default = 0)
   totalPrestiges = ndb.IntegerProperty(default = 0)
+
+  def OrderSet(self, value):
+    if not isinstance(value, Order):
+      raise TypeError('Expected an Enum, received: %s' % (value,))
+    if value == Order.HI_LEVEL_ASC:
+      super(TapGameInfo, self).OrderSet('highestLevel')
+    elif value == Order.HI_LEVEL_DESC:
+      super(TapGameInfo, self).OrderSet('-highestLevel')
+    elif value == Order.TOTAL_PRES_ASC:
+      super(TapGameInfo, self).OrderSet('totalPrestiges')
+    elif value == Order.TOTAL_PRES_DESC:
+      super(TapGameInfo, self).OrderSet('-totalPrestiges')
+    elif value == Order.TOTAL_REL_ASC:
+      super(TapGameInfo, self).OrderSet('totalRelics')
+    elif value == Order.TOTAL_REL_DESC:
+      super(TapGameInfo, self).OrderSet('-totalRelics')
+    else:
+      raise TypeError('Expected an Enum, received: %s' % (value,))
+
+  @EndpointsAliasProperty(setter=OrderSet, property_type=Order, default=DEFAULT_ORDER)
+  def order(self):
+    return super(TapGameInfo, self).order
 
   @staticmethod
   def insert(new_tapgameinfo):
@@ -19,5 +56,11 @@ class TapGameInfo(EndpointsModel):
       return True
     return False
 
-#   @staticmethod
-#   def update()
+  @staticmethod
+  def getByUserId(uId):
+#     gameInfo = TapGameInfo.query(ancestor = ndb.Key(TapUser, uId))
+    user = TapUser.query(TapUser.userID == uId).get()
+    if user is not None:
+      gameInfo = TapGameInfo.query(ancestor = user.key)
+      return gameInfo
+    return None
